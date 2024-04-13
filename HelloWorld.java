75938511ff541946f237236094c1fdf81e88c9ed
NguyenArmy <anhquan109380@gmail.com>
2024-04-13 08:57:38 +0700

    init project

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package bai1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("anh quân ");
+		System.out.println("một hai ba");
+
+	}
+}
